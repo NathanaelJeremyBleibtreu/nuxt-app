f0dbda858c6fdfc01e5bfc209c504c213e1cb37e
URIT <urit@2a02-8428-27d6-7901-86e6-98df-c06e-1835.rev.sfr.net>
2023-02-23 20:39:24 +0100

    add index page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>INDEX</h1>
+        <button @click="inc">{{ count }} clicks</button>
+    </div>
+</template>
+
+<script setup>
+
+const count = ref(0)
+const inc = ()=>{
+    count.value++
+}
+
+</script>
